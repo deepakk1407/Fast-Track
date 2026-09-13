79f80b605aeef7909da33add19ef271da10155ed
deepakk1407 <deepakk2k07@gmail.com>
2026-09-13 15:07:29 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 alert("FAST-TRACK JS WORKING");
+
+const loginTab = document.getElementById("loginTab");
+const registerTab = document.getElementById("registerTab");
+
+const loginBox = document.getElementById("loginBox");
+const registerBox = document.getElementById("registerBox");
+
+registerTab.addEventListener("click", () => {
+    loginBox.style.display = "none";
+    registerBox.style.display = "block";
+});
+
+loginTab.addEventListener("click", () => {
+    registerBox.style.display = "none";
+    loginBox.style.display = "block";
+});
