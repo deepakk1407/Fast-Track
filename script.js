6e5be8d3b482a97721df4745c66056291a71a8ab
deepakk1407 <deepakk2k07@gmail.com>
2026-09-14 19:02:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const registerButton = document.getElementById("registerButton");
-
-console.log("Register Button:", registerButton);
-
-if (registerButton) {
-    registerButton.addEventListener("click", function () {
-        alert("REGISTER BUTTON CLICKED 🔥");
-    });
-} else {
-    alert("REGISTER BUTTON NOT FOUND ❌");
-}
+alert("STEP 6 TEST - NEW JAVASCRIPT");
