a5b3bd2ab9f6184cfebe9949d7e5d560bee9ee90
deepakk1407 <deepakk2k07@gmail.com>
2026-09-14 19:03:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("STEP 6 TEST - NEW JAVASCRIPT");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const registerButton = document.getElementById("registerButton");
+
+    alert(
+        registerButton
+            ? "REGISTER BUTTON FOUND ✅"
+            : "REGISTER BUTTON NOT FOUND ❌"
+    );
+
+    if (registerButton) {
+
+        registerButton.addEventListener("click", function () {
+
+            alert("REGISTER BUTTON CLICKED 🔥");
+
+        });
+
+    }
+
+});
