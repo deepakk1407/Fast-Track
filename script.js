85dacf573b4b0c92ffa9c4ba99773bf1b725ac41
deepakk1407 <deepakk2k07@gmail.com>
2026-09-14 19:01:08 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("FAST-TRACK JAVASCRIPT WORKING");
+const registerButton = document.getElementById("registerButton");
+
+console.log("Register Button:", registerButton);
+
+if (registerButton) {
+    registerButton.addEventListener("click", function () {
+        alert("REGISTER BUTTON CLICKED 🔥");
+    });
+} else {
+    alert("REGISTER BUTTON NOT FOUND ❌");
+}
